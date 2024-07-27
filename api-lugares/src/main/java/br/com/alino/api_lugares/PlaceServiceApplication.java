21a26f754113592dffec05fe959efdc058220d3f
AntonioALino <antonioaplino@gmail.com>
2024-07-27 09:24:50 -0300

    Starting project

---
+++ api-lugares/src/main/java/br/com/alino/api_lugares/PlaceServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.alino.api_lugares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceServiceApplication.class, args);
+	}
+
+}
